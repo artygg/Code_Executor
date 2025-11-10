c2917191fb3e8737a5e2613dae8ce1a8c4456b5e
artyomgrishayev <artjoms.grisajevs@student.nhlstenden.com>
2025-11-10 18:59:14 +0100

    Add: queue and memory

---
+++ internal/queue/memory/memory.go
@@ -0,0 +1,60 @@
+package memory
+
+import (
+	"Code_executor/internal/queue"
+	"context"
+	"errors"
+	"fmt"
+)
+
+type InMemoryQueue struct {
+	ch chan queue.Job
+}
+
+var (
+	ErrInvalidMemoryBufferSize = errors.New("invalid memory buffer size")
+)
+
+func NewInMemoryQueue(buffer int) (*InMemoryQueue, error) {
+	if buffer <= 0 {
+		return nil, ErrInvalidMemoryBufferSize
+	}
+	return &InMemoryQueue{ch: make(chan queue.Job, buffer)}, nil
+}
+
+func (q *InMemoryQueue) Enqueue(ctx context.Context, job queue.Job) error {
+	if job.ExecutionID == "" {
+		return fmt.Errorf("execution id is empty")
+	}
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case q.ch <- job:
+		return nil
+	}
+}
+
+func (q *InMemoryQueue) Consume(ctx context.Context) (<-chan queue.Job, error) {
+	out := make(chan queue.Job)
+
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case job, ok := <-q.ch:
+				if !ok {
+					return
+				}
+				select {
+				case <-ctx.Done():
+					return
+				case out <- job:
+				}
+			}
+		}
+	}()
+	return out, nil
+}
---
+++ internal/queue/queue.go
@@ -0,0 +1,17 @@
+package queue
+
+import "context"
+
+type Job struct {
+	ExecutionID string
+	Language    string
+	UserID      string
+}
+
+type Producer interface {
+	Enqueue(ctx context.Context, job *Job) error
+}
+
+type Consumer interface {
+	Consume(ctx context.Context) (<-chan Job, error)
+}
